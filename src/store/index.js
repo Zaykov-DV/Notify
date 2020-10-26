1eca767f9e58da0dfeebfdd5341f268d4723dfb0
Дмитрий <grunger.ekb@gmail.com>
2020-10-26 10:56:03 +0500

    added files in store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import notify from './notify'
+import errors from './errors'
+import loading from './loading'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        notify
+        notify, errors, loading
     }
 })
--- src/store/notify.js
+++ src/store/notify.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import loadMore from "../assets/js/loadMore";
 
 export default {
@@ -26,7 +27,39 @@ export default {
         loadMessages ({commit, getters}) {
             let res = getters.getMessageFilter
             commit('loadMessages', loadMore(res))
-        }
+        },
+        // dispatch from loading.js to -- NotifyPage.vue
+        setNotifyLazy ({commit, dispatch}) {
+            commit('setLoading', true)
+            setTimeout(() => {
+                dispatch('setLoading')
+            }, 1800)
+        },
+        // loading
+        setLoading({commit}) {
+            // this.loading = true
+            axios
+                .get('https://www.eclipsesite.site/api/notifyApi.php')
+                .then(response => {
+                    let res = response.data.notify,
+                        messages = [],
+                        messagesMain = []
+
+                    //filter
+                    for (let i = 0; i < res.length; i++) {
+                        if (res[i].main) messagesMain.push(res[i])
+                        else messages.push(res[i])
+                    }
+
+                    commit('setMessage', messages)
+                    commit('setMessageMain', messagesMain)
+                })
+                .catch(error => {
+                    console.log(error)
+                    commit('setError', 'Error: Network Error')
+                })
+                .finally( () => commit('setLoading', false) )
+        },
     },
     getters: {
         getMessage(state) {
